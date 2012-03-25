a9b7f3d7de02cfd9ea7bee4c4781240f3455a3a6
Christopher Theberge <c.theberge@codeforyou.com>
2012-03-25 12:09:22 -0400

    Adding more missing precompile assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ ThebergestoneweddingCom::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['style.css', 'reset.css', 'grid.css']
+  config.assets.precompile += ['style.css', 'reset.css', 'grid.css', 'cufon-yui.js', 'english.cufonfonts.js', 'jquery-1.7.1.min.js', 'html5.js', 'jquery.easing.1.3.js', 'jquery.galleriffic.js', 'jquery.opacityrollover.js', 'tms_presets.js', 'tms-0.3.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
