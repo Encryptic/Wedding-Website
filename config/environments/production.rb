65b2821ef7bb9676264cd71221b15744f222ea6c
Christopher Theberge <c.theberge@codeforyou.com>
2012-03-25 12:02:37 -0400

    Cleaning up some asset compilation issues.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ ThebergestoneweddingCom::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['style.css', 'reset.css', 'grid.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
