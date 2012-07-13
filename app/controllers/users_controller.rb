2cd2a9c3ccfb48c66a138e1d8460aa72a40212e2
Christopher Theberge <c.theberge@codeforyou.com>
2012-07-12 22:52:55 -0400

    Added a link to show a user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+  #before_filter :auth_required
+  
   def index
     @users = User.all
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @users }
+    end
+  end
+  
+  def show
+    @user = current_user
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @pages }
