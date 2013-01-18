16842e019bf16843a44660a22126964e2095164c
Christopher Theberge <c.theberge@codeforyou.com>
2013-01-18 10:57:11 -0500

    Minor code cleanup

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -10,7 +10,6 @@ class ConfirmationMailer < ActionMailer::Base
 
   def admin_email(guest)
     @guest = guest
-
     mail(:to => "wedding@thebergestonewedding.com", :subject => "#{@guest.first_name} #{@guest.last_name} RSVP Report")
   end
 end
